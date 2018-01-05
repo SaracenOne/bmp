7e99a18c6fc40c9ed5d4771fdb6a7be20ea11469
Saracen <SaracenOne@gmail.com>
2018-01-05 09:19:41 +0000

    Godot 3 update.

--- image_loader_bmp.h
+++ image_loader_bmp.h
@@ -1,52 +1,55 @@
-#ifndef IMAGE_LOADER_TGA_H
-#define IMAGE_LOADER_TGA_H
+#ifndef IMAGE_LOADER_BMP_H
+#define IMAGE_LOADER_BMP_H
 
 #include "io/image_loader.h"
 
 class ImageLoaderBMP : public ImageFormatLoader {
 protected:
-	static const unsigned BITMAP_SIGNATURE = 0x4d42;
+  static const unsigned BITMAP_SIGNATURE = 0x4d42;
 
-	struct bmp_header_s
-	{
-		struct bmp_file_header_s {
-			uint16_t bmp_signature;
-			uint32_t bmp_file_size;
-			uint32_t bmp_file_padding;
-			uint32_t bmp_file_offset;
-		} bmp_file_header;
+  struct bmp_header_s {
+    struct bmp_file_header_s {
+      uint16_t bmp_signature;
+      uint32_t bmp_file_size;
+      uint32_t bmp_file_padding;
+      uint32_t bmp_file_offset;
+    } bmp_file_header;
 
-		struct bmp_info_header_s {
-			uint32_t bmp_header_size;
-			uint32_t bmp_width;
-			uint32_t bmp_height;
-			uint16_t bmp_planes;
-			uint16_t bmp_bit_count;
-			uint32_t bmp_compression;
-			uint32_t bmp_size_image;
-			uint32_t bmp_pixels_per_meter_x;
-			uint32_t bmp_pixels_per_meter_y;
-			uint32_t bmp_colors_used;
-			uint32_t bmp_important_colors;
-			uint32_t bmp_red_mask;
-			uint32_t bmp_green_mask;
-			uint32_t bmp_blue_mask;
-			uint32_t bmp_alpha_mask;
-			uint32_t bmp_cs_type;
-			uint32_t bmp_endpoints[9];
-			uint32_t bmp_gamma_red;
-			uint32_t bmp_gamma_green;
-			uint32_t bmp_gamma_blue;
+    struct bmp_info_header_s {
+      uint32_t bmp_header_size;
+      uint32_t bmp_width;
+      uint32_t bmp_height;
+      uint16_t bmp_planes;
+      uint16_t bmp_bit_count;
+      uint32_t bmp_compression;
+      uint32_t bmp_size_image;
+      uint32_t bmp_pixels_per_meter_x;
+      uint32_t bmp_pixels_per_meter_y;
+      uint32_t bmp_colors_used;
+      uint32_t bmp_important_colors;
+      uint32_t bmp_red_mask;
+      uint32_t bmp_green_mask;
+      uint32_t bmp_blue_mask;
+      uint32_t bmp_alpha_mask;
+      uint32_t bmp_cs_type;
+      uint32_t bmp_endpoints[9];
+      uint32_t bmp_gamma_red;
+      uint32_t bmp_gamma_green;
+      uint32_t bmp_gamma_blue;
 
-		} bmp_info_header;
-	};
+    } bmp_info_header;
+  };
 
-	static Error ImageLoaderBMP::convert_to_image(Image *p_image, const uint8_t *p_buffer, const uint8_t *p_color_buffer, const bmp_header_s &p_header);
+  static Error ImageLoaderBMP::convert_to_image(Ref<Image> p_image,
+                                                const uint8_t *p_buffer,
+                                                const uint8_t *p_color_buffer,
+                                                const bmp_header_s &p_header);
 
 public:
-	virtual Error load_image(Image *p_image,FileAccess *f);
-	virtual void get_recognized_extensions(List<String> *p_extensions) const;	
-	ImageLoaderBMP();
+  virtual Error load_image(Ref<Image> p_image, FileAccess *f,
+                           bool p_force_linear, float p_scale);
+  virtual void get_recognized_extensions(List<String> *p_extensions) const;
+  ImageLoaderBMP();
 };
 
-#endif
+#endif // IMAGE_LOADER_BMP_H
--- register_types.cpp
+++ register_types.cpp
@@ -6,11 +6,8 @@ static ImageLoaderBMP *image_loader_bmp = NULL;
 
 void register_bmp_types() {
 
-	image_loader_bmp = memnew(ImageLoaderBMP);
-	ImageLoader::add_image_format_loader(image_loader_bmp);
+  image_loader_bmp = memnew(ImageLoaderBMP);
+  ImageLoader::add_image_format_loader(image_loader_bmp);
 }
 
-void unregister_bmp_types() {
-
-	memdelete(image_loader_bmp);
-}
+void unregister_bmp_types() { memdelete(image_loader_bmp); }
